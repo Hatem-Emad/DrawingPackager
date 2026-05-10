c758bc0a2bae1bb2d0ef3a53e87f9eabf78a564a
HatemEmad <hatememadelden@gmail.com>
2026-05-10 22:50:23 +0300

    Add project files.

---
+++ DrawingPackager.CLI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DrawingPackager.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
